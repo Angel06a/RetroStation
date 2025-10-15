16978af0b1c1de669b5d30b1764766418b3a7968
Angel06 <ug25228@gmail.com>
2025-10-14 23:46:19 -0500

    Add files via upload

---
+++ Games/naomi2.js
@@ -0,0 +1,5 @@
+// Usamos una variable genérica. La lógica en index.html se encargará de leerla
+// y eliminarla inmediatamente después de su uso para prevenir bugs.
+window.currentGameListString = `
+-Virtua Fighter 4 Evolution "https://download843.mediafire.com/rv6ack10f4bgXtMCPToCdQugEyzxr9oVTHfFfkYDp4PbTpCe3zJoLR9ez4whVXsYII_-o6EbXR8aeVFJXLkGQa1K5mfbn7x_kmpYM8mujUrxFnhsys7s6vv2yfyiWqeGHZgAo7FVvgbddopxTCWBzLorNJOFx-UadJC81GercGU/gyvkhbuqs1gk54d/vf4evoct.rar"
+`;
