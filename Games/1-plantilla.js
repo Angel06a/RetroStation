e07c19666760ba0a42c0276f06463d2b555b8463
Angel06 <ug25228@gmail.com>
2025-10-14 23:45:36 -0500

    Add files via upload

---
+++ Games/1-plantilla.js
@@ -0,0 +1,5 @@
+// Usamos una variable genérica. La lógica en index.html se encargará de leerla
+// y eliminarla inmediatamente después de su uso para prevenir bugs.
+window.currentGameListString = `
+-
+`;
