c8596b62abe79abf96cf0cda84c087124f4eff3d
Angel06 <ug25228@gmail.com>
2025-10-15 21:41:27 -0500

    Delete Games/snes-msu.js

--- Games/snes-msu.js
+++
@@ -1,5 +0,0 @@
-// Usamos una variable genérica. La lógica en index.html se encargará de leerla
-// y eliminarla inmediatamente después de su uso para prevenir bugs.
-window.currentGameListString = `
--Killer Instinct MSU-1 "https://download1586.mediafire.com/f5b0mx5v8hegsZ278dagfLT2XQNQgoUAEj0Q6TAbX1g6OBjjEv-vBGxflJkhlD6TzmoSyGVZ7tATEpN1oNmLzX74gT0Yk44nAFNzn_kSs83gUt3z5S_9tewgjLde790g1eAdMz9FFtgT9DjV9WauAug881JR4NWgxV_0aMROHxA/kl6nvj0e2ztcxu8/Killer+Instinct+MSU-1.zip"
-`;
