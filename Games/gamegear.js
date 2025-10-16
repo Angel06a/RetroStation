9188ebb627040d126a233d22922ce6414e028a11
Angel06 <ug25228@gmail.com>
2025-10-15 21:40:56 -0500

    Delete Games/gamegear.js

--- Games/gamegear.js
+++
@@ -1,8 +0,0 @@
-// Usamos una variable genérica. La lógica en index.html se encargará de leerla
-// y eliminarla inmediatamente después de su uso para prevenir bugs.
-window.currentGameListString = `
--Fatal Fury Special (Europe) "https://download1580.mediafire.com/uoo769gkljggvRY8aBHci4yml9yQt8kLOGa-t2yE6nXBgzHREdCfB5gHJyQlx-yBKj3UN6iwILPWQwq8z_rUzgPAajyd8OZegMsJ86IsyS4JXa6zmP1IizZABWCp2vpDVuqtdB1dadNEqmtwkXrV9aQoyYv8gX2ax2zqrcK4iQ0/jwdl61lhyjcvozn/Fatal+Fury+Special+%28Europe%29.gg"
--Mega Man (USA) "https://download1655.mediafire.com/cy5flqddn04gtBh3zuaIE1z1ZSMVStfm6KuFq20QofCoRM2GaWg2ZLE86G8waRS4emExInCAgDlnCJ2EDpTXuWrounCfllzuE1QzCCJ0fE5mDwliYJEwyfqzV5HARY5TH4SKcwE-jqi3easziwzXIVeynm4UHI52VcdxfDc0Uwc/9ydr1e2olf616h3/Mega+Man+%28USA%29.gg"
--Sonic Drift 2 (World) "https://download1472.mediafire.com/nacjq56n3yngbYmkgLhtfMjAsMismOenJSqjUboxr_GVyGfX86Km2r5ARJdu-nm0xlLllyfFMHwzs4qsneCBn2-6WXQLyQwkUSVdIy-FbKERxv-xMPExv-C3wZW7YwmWv3q2hFmmr3UPqc2WY1mS9EsdeQ0E1SBirOUbB-ECEF0/5za4mfzqwlaajt3/Sonic+Drift+2+%28World%29.gg"
--Sonic The Hedgehog - Triple Trouble (USA, Europe, Brazil) (En) "https://download1322.mediafire.com/ax0lhe4bujqgvWSCAfzRu2t8yWlm-VPZRstcw2W5kFc-DHopUCBtZ_LqsesMRyPUtZiamtb4B4KF9VDxLhjOQ3Iaz1rVKx6Mj4ay8oxjw0zJrvBpPpErhPd3vU_A4iiDrzcg5bGiskCnqO7tGKKWIY1p47dHAW5adYhr2i-qKKA/6dzq0vvmzneu87a/Sonic+The+Hedgehog+-+Triple+Trouble+%28USA%2C+Europe%2C+Brazil%29+%28En%29.gg"
-`;
