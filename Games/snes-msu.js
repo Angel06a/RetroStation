650b3b8d2eb9688bdbb87434e035189a697f5bf1
Angel06 <ug25228@gmail.com>
2025-10-15 21:44:45 -0500

    Add files via upload

---
+++ Games/snes-msu.js
@@ -0,0 +1,5 @@
+// Usamos una variable genérica. La lógica en index.html se encargará de leerla
+// y eliminarla inmediatamente después de su uso para prevenir bugs.
+window.currentGameListString = `
+-Killer Instinct MSU-1 "https://www.mediafire.com/file/kl6nvj0e2ztcxu8/Killer+Instinct+MSU-1.zip"
+`;
