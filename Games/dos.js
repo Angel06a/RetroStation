da8acef04d468aea4c6ea31eb687893508c9a1c6
Angel06 <ug25228@gmail.com>
2025-10-15 21:41:19 -0500

    Delete Games/dos.js

--- Games/dos.js
+++
@@ -1,6 +0,0 @@
-// Usamos una variable genérica. La lógica en index.html se encargará de leerla
-// y eliminarla inmediatamente después de su uso para prevenir bugs.
-window.currentGameListString = `
--Blockout "https://download1979.mediafire.com/p9oqibac454giMoWEBEpod2bolv5FA959TTtACQJSMq_nxQzewpkpxWPbGg774mAAbjHeXDhevkVsDAoaDGWHrnCbCxocIVvsK8dYOkWdsZW6XkAE4-O828WWtJcKZAhkoiheYykU4D5exoVWcCAfhEIlNA5Lu5VJDjW1NZgmQU/3ap87swgpa9hqv7/Blockout.zip"
--DOOM "https://download1655.mediafire.com/il0vuigowalgS8mI7p2hlm56N8h5l9tIhDshlrCulnLmD_d3bDlOu9Zb76wBSOMrj6zTAgEpBdMUNboKqPIeJSu5ectmC6xUJRaA_IucQ_QNKS9Hc6mX9U3E2fW65JyWD5CcU-zjuFCdbfCzZ_45twrUr5Ux0bNO34CvvjKCze8/ptiwh0ij5gqrthy/DOOM.zip"
-`;
