921af3789fd41fa35cae29999424ba2789ab6a23
Angel06 <ug25228@gmail.com>
2025-10-15 21:39:59 -0500

    Delete Games/saturn.js

--- Games/saturn.js
+++
@@ -1,9 +0,0 @@
-// Usamos una variable genérica. La lógica en index.html se encargará de leerla
-// y eliminarla inmediatamente después de su uso para prevenir bugs.
-window.currentGameListString = `
--Fighters Megamix (USA) "https://download1530.mediafire.com/qx7iwpt6bhrgZCP3W8ImxgLxugF_7z6-o52Uz19-RJNofJVcUKtI7mMeKe_H_6GZZKzbaRO1SUxMn5G0UvimleubR6GJPbneDGAV79yIr3w7fm4lAsXoNQmWfUkXdHEOaaASyOaYWj5SjBqBNA6hpA3V2uyeOWqyrQvk0NrLmRQ/c6fu7yv1gqjbz63/Fighters+Megamix+%28USA%29.chd"
--Sonic R (USA, Brazil) "https://download1531.mediafire.com/1tc5l22oczsg5E-yKz6s-2Tw_yO_o4DRQT98VftRcW1u_v_qHSQ1pquBfBefy4PjeJl1ZAjoSpe6Oel9Lhb-BzkhKWFHP58GzboJdbHSZhzFWDPCBzWJ8o4jTIJi6YeXP1YQhqBIbDTKUeyb-ZDfSrx2DoqNoZyy7Lj2MdJxCSo/ikqb9lk50q4whzn/Sonic+R+%28USA%2C+Brazil%29.chd"
--Virtua Fighter (USA) "https://download1588.mediafire.com/ckrow7dpmewgt_a063-qXpjBWhyAe-szt8kqsHhsObkqAL4fP4A-vMqj874h7-8NV-JrskE3FuVegWq7mccimEMiY_-w5pCrdFHmR9yJeS6q4o-wuy7FstnfnXlrEHz8KXm6jyxm1zv-C_LG7X8nQeauWIDjGj840uO4SIZ6rcw/pmzfs9d1baovxdv/Virtua+Fighter+%28USA%29.chd"
--Virtua Fighter 2 (USA) "https://download1334.mediafire.com/9nrjof02gdsgJ8tol9W-5dXr3ZUbURVEDnyYCZCX_64i_h4saimYDknvSH1owge3Snd445FALod5C4TKLm5jT8JuilDlLhxP6gRRcE3jTJ-RX-sFNsltkY6KJzUO5m8rYQiKW7q5mhe5NhF9JTVcLdPlcn5usntNyKqYpoRteI8/zi6valc6dfed6lk/Virtua+Fighter+2+%28USA%29.chd"
--Virtua Fighter Remix (USA) "https://download1654.mediafire.com/xkmedkesp8mg3vcrqbsDHQkDDpwhYCux0RoOrNYTiTNduD16Aau10pkllH8lSK3ECfotgfCOc7ZJs_jz_V-4mg8jKU7BYqPDG2N9dKEFnl_UuQLTsSb-y3DY0nK8bagZyBfR9W27gaicONX6Upk0W7M0DlBegUz8sjxuOdAVwlA/yhc7c8nea8wgguo/Virtua+Fighter+Remix+%28USA%29.chd"
-`;
